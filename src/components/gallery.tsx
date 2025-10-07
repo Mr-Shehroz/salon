cdc383450ae14779fb9dd7aee8494d9766692df2
Mr-Shehroz <shehrozkhan.wow@gmail.com>
2025-10-07 11:06:51 +0500

    minor change of padding in the gallery section

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Gallery = () => {
     return (
-        <section className='bg-white lg:pt-20 pt-10 px-5 2xl:px-0'>
+        <section className='bg-white pt-10 px-5 2xl:px-0'>
             <h2 className='text-[50px] text-center lg:pt-10 pt-5 lg:pb-20 pb-10'>Gallery</h2>
             <div className='flex justify-center items-center flex-wrap lg:flex-nowrap'>
                 <Image src="/1.png" alt="Gallery Image 1" width={378} height={200} className="2xl:w-[378px] xl:w-[250px] md:w-[204px] w-full" />
